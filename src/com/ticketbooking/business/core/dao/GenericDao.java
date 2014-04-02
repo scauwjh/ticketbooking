6da381cf5490112865b8c0019e753a33aa80ed68
scauwjh <scauwjh@gmail.com>
2014-04-03 00:25:50 +0800

    change the name of the dao
    Signed-off-by: scauwjh <scauwjh@gmail.com>

--- src/com/ticketbooking/business/core/dao/GenericDao.java
+++ src/com/ticketbooking/business/core/dao/GenericDao.java
@@ -9,15 +9,15 @@ import com.ticketbooking.util.HibernateUtil;
  * @author wjh E-mail: 472174314@qq.com
  * @version 创建时间：2014年3月27日 下午8:36:22 
  */
-public class GeneralDao implements IGeneralDao {
+public class GenericDao implements IGenericlDao {
 
 	// Google工程师BobLee发明的饱（懒）汉单例模式。解决了饱汉单例模式两大难题。
 	private static class SingletonInstance {
-		private static final GeneralDao instance = 
-				new GeneralDao();
+		private static final GenericDao instance = 
+				new GenericDao();
 	}
 	
-	public static GeneralDao getInstance() {
+	public static GenericDao getInstance() {
 		return SingletonInstance.instance;
 	}
 	
--- src/com/ticketbooking/business/core/dao/IGenericlDao.java
+++ src/com/ticketbooking/business/core/dao/IGenericlDao.java
@@ -7,7 +7,7 @@ import java.util.List;
  * @author wjh E-mail: 472174314@qq.com
  * @version 创建时间：2014年3月27日 下午8:31:41 
  */
-public interface IGeneralDao {
+public interface IGenericlDao {
 	
 	/**
 	 * general saveOrUpdate
